3f0096ba53c15343b73a14784bc2cb3a8c32440d
liugang <liugang17@126.com>
2019-12-12 18:16:27 +0800

    feat: 1、代码优化

--- weiye-admin/src/main/java/com/ruoyi/web/controller/system/SysOssController.java
+++ weiye-admin/src/main/java/com/ruoyi/web/controller/system/SysOssController.java
@@ -26,7 +26,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
--- weiye-admin/src/main/java/com/ruoyi/web/controller/tool/UploadFileController.java
+++ weiye-admin/src/main/java/com/ruoyi/web/controller/tool/UploadFileController.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
